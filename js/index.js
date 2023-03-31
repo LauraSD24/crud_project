4b8b76980fd4472467a36db0f4611d24b3e2bf33
Laura_1998 <laurasaldel24@gmail.com>
2023-03-30 23:08:46 -0500

    agregando cambios en media querys

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
-
 window.addEventListener("DOMContentLoaded", () => {
-    let changeBtn = false;
     const array_input = document.querySelectorAll(".input_form");
     const container_users_registred = document.querySelector(".container_users_registred");
     const btn_save_user = document.querySelector(".btn_save_user");
     const btn_update = document.querySelector(".btn_update");
+    const btn_cancel_update = document.querySelector(".btn_cancel_update");
     const form = document.querySelector(".form");
     const number_users_registred = document.querySelector(".number_users_registred");
     const input_search = document.querySelector(".input_search");
-
+    const icon_delete_all = document.querySelector(".fa-solid");
+    const container_registers = document.querySelector(".container_registers");
+    
+    let changeBtn = false;
     let array_users = [];
-    let array_local = JSON.parse(localStorage.getItem("users")) || [];
-    // let array_local_actualiced = JSON.parse(localStorage.getItem("users")) || [];
+    let array_local;
+    let array_local_updated = JSON.parse(localStorage.getItem("users")) || [];
 
-    get_objects_localstorage(array_local);
+    get_objects_localstorage(array_local_updated);
     evenBtns();
     // eventSave();
 
@@ -25,7 +27,6 @@ window.addEventListener("DOMContentLoaded", () => {
                 alert("¡Campos vacíos! Complete toda la información");
             } else {
                 create_user();
-                get_objects_localstorage(array_local);
                 evenBtns();
             }
         });
@@ -37,7 +38,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     function create_user() {
 
-        // array_local = JSON.parse(localStorage.getItem("users")) || [];
+        array_local = JSON.parse(localStorage.getItem("users")) || [];
 
         const userFind = array_local.find((u) => array_input[2].value === u.document) || "";
         console.log(userFind);
@@ -46,19 +47,22 @@ window.addEventListener("DOMContentLoaded", () => {
             alert("El documento ingresado ya existe");
         }else{
             const object_info = {
-                name: `${array_input[0].value}`,
-                lastname: `${array_input[1].value}`,
-                document: `${array_input[2].value}`,
-                phone: `${array_input[3].value}`,
-                email: `${array_input[4].value}`,
+                name: `${array_input[0].value.toLowerCase().trim()}`,
+                lastname: `${array_input[1].value.toLowerCase().trim()}`,
+                document: `${array_input[2].value.trim()}`,
+                phone: `${array_input[3].value.trim()}`,
+                email: `${array_input[4].value.toLowerCase().trim()}`,
             }
     
             if (array_local.length > 0) {
                 array_local.unshift(object_info);
                 save_user_localstorage();
+                get_objects_localstorage(array_local);
+                console.log("local")
             } else {
                 array_users.unshift(object_info);
                 localStorage.setItem("users", JSON.stringify(array_users));
+                get_objects_localstorage(array_users);
             }
             form.reset();
         }
@@ -67,7 +71,7 @@ window.addEventListener("DOMContentLoaded", () => {
     function get_objects_localstorage(array) {
 
         container_users_registred.innerHTML = "";
-        // array_local = JSON.parse(localStorage.getItem("users")) || [];
+        array_local = JSON.parse(localStorage.getItem("users")) || [];
 
         array.map((value) => {
             const card = document.createElement("div");
@@ -115,7 +119,7 @@ window.addEventListener("DOMContentLoaded", () => {
         let comfirm_delete = confirm("¿Seguro que desea eliminar el usuario?");
 
         if (comfirm_delete) {
-            // array_local = JSON.parse(localStorage.getItem("users"));
+            array_local = JSON.parse(localStorage.getItem("users"));
             for (let i = 0; i < array_local.length; i++) {
                 if (array_local[i].document === document_event) {
                     array_local.splice(i, 1);
@@ -129,10 +133,9 @@ window.addEventListener("DOMContentLoaded", () => {
     };
 
     function evenBtns() {
-        const array_btn_update = [...document.querySelectorAll(".btn_update_user")]
-        const array_btn_delete = [...document.querySelectorAll(".btn_delete_user")]
+        const array_btn_update = [...document.querySelectorAll(".btn_update_user")];
+        const array_btn_delete = [...document.querySelectorAll(".btn_delete_user")];
         console.log(array_btn_delete)
-        // console.log(array_btn_update)
 
         for (let i = 0; i < array_btn_delete.length; i++) {
             const btn = array_btn_delete[i];
@@ -140,21 +143,22 @@ window.addEventListener("DOMContentLoaded", () => {
                 const id = e.currentTarget.id;
                 delete_user(id)
                 console.log("delete");
-            })
+            });
         }
         for (let i = 0; i < array_btn_update.length; i++) {
             const btn = array_btn_update[i];
             btn.addEventListener("click", (e) => {
                 const id = e.currentTarget.id;
                 update_user(id);
-                // btn_update.addEventListener("click",()=>btn_update_user(update_user(id),e));
-            })
+            });
         }
         // eventSave();
     }
 
     function update_user(document_event) {
-        // array_local = JSON.parse(localStorage.getItem("users"));
+        array_local = JSON.parse(localStorage.getItem("users"));
+        container_registers.style.pointerEvents = "none";
+        // container_registers.style.backgroundColor = 
 
         const userFind = array_local.find((u) => document_event === u.document);
         let values_object = array_local[array_local.indexOf(userFind)];
@@ -170,6 +174,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
         btn_save_user.classList.add("btn_ocult");
         btn_update.classList.remove("btn_ocult");
+        btn_cancel_update.classList.remove("btn_ocult");
         btn_update.setAttribute("id", indexObject);
         return indexObject;
     };
@@ -187,10 +192,10 @@ window.addEventListener("DOMContentLoaded", () => {
 
             console.log(indexObject);
 
-            array_local[indexObject].name = array_input[0].value;
-            array_local[indexObject].lastname = array_input[1].value;
-            array_local[indexObject].phone = array_input[3].value;
-            array_local[indexObject].email = array_input[4].value;
+            array_local[indexObject].name = array_input[0].value.toLowerCase().trim();
+            array_local[indexObject].lastname = array_input[1].value.toLowerCase().trim();
+            array_local[indexObject].phone = array_input[3].value.trim();
+            array_local[indexObject].email = array_input[4].value.toLowerCase().trim();
 
             if (validate_form()) {
                 alert("¡Campos vacíos! Complete toda la información")
@@ -201,7 +206,9 @@ window.addEventListener("DOMContentLoaded", () => {
                 form.reset();
                 btn_save_user.classList.remove("btn_ocult");
                 btn_update.classList.add("btn_ocult");
+                btn_cancel_update.classList.add("btn_ocult");
                 array_input[2].removeAttribute("readOnly");
+                container_registers.style.pointerEvents = "visible";
                 changeBtn = false;
             }
             evenBtns();
@@ -210,13 +217,39 @@ window.addEventListener("DOMContentLoaded", () => {
 
     function search(word_filter) {
         // array_local = JSON.parse(localStorage.getItem("users"));
-        const array_filter = array_local.filter((u) => u.name.includes(word_filter) || u.lastname.includes(word_filter));
-        get_objects_localstorage(array_filter);
+        console.log(array_local);
+    
+        const array_filter = array_local.filter((u) => (u.name + " " + u.lastname).includes(word_filter))
+            // const fullname = u.name + " " + u.lastname;
+            // console.log(fullname)
+            // fullname.includes(word_filter)
+        // });
+        console.log(array_filter)
+        
+        if(array_filter.length > 0){
+            get_objects_localstorage(array_filter);
+            container_users_registred.classList.remove("container_users_registred_not_found");
+        }else{
+            container_users_registred.innerHTML = "No se encontraron registros";
+            container_users_registred.classList.add("container_users_registred_not_found");
+        }
     }
 
     input_search.addEventListener("input", (e)=>{
         e.preventDefault();
-        search(input_search.value);
+        search(input_search.value.toLowerCase());
         evenBtns();
     })
+
+    function delete_all() {
+        let verify = confirm("¿Está seguro de eliminar todos los usuarios?");
+
+        if(verify){
+            localStorage.removeItem("users");
+            array_local = JSON.parse(localStorage.getItem("users")) || [];
+            get_objects_localstorage(array_local);
+        }
+    }
+
+    icon_delete_all.addEventListener("click",delete_all);
 });
